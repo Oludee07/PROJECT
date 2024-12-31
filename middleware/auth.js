9039e5cdf9eb6d77369066c99eb80bdf0d1054c9
oludolapo <dolapoja@gmail.com>
2024-12-31 19:19:48 +0100

    authorization2

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,16 +2,18 @@ const User = require("../models/User");
 const jwt = require("jsonwebtoken");
 
 const validateToken = (req, res, next) => {
-    const token = req.headers['auhtorization'];
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]
     console.log('token:', token);
 
-    if (!token) {
+    if (!token || token==="") {
         res.status(401).json({ message: 'You must be logged in to access this route'});
         return;
     }
 
-    jwt.verify(token, process.env.JWT_SECRET, async (error, payload) => {
+    jwt.verify(token, process.env.JWT_SECRET_KEY, async (error, payload) => {
         if(error) {
+            console.error('JWT Error:', error.message);
             res.status(401).json({ message: 'You must be logged in to access this route' });
             return;
         }
